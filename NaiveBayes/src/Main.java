e6fce4d86453b29a068b817ecda76183f7fff7ff
elenidriva <ele_1924@hotmail.com>
2018-12-25 19:46:35 +0200

    Training works correctly. Classification: Needs some changes probably
    Trained on different datasets.
    *Enron DataSet*
    Ham Accuracy ~ 99% (771/778)
    Spam Accuracy ~ 77% (95/123)

--- NaiveBayes/src/Main.java
+++ NaiveBayes/src/Main.java
@@ -7,9 +7,103 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		NaiveBayes NBSpam = new NaiveBayes();
-        File file = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/spam");
-        File[] files = file.listFiles();
+		
+        File spamFolder = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/spamTrain");
+		//File spamFolder = new File("C:/Users/ele_1/ML/NaiveBayes/part1/spam");
+        File[] spamFiles = spamFolder.listFiles();
+        for(File f: spamFiles) {
+        	 NaiveBayes NBSpam = new NaiveBayes(Category.Spam);
+        	System.out.println(f);
+        	Scanner s = new Scanner(f);
+        	ArrayList<String> list = new ArrayList<String>();
+    		while (s.hasNext()){
+    		    list.add(s.next());
+    		}
+    		s.close();
+    		
+    		NBSpam.train( list, Category.Spam);
+    		
+        }
+        
+       File folder = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/hamTrain");
+     //   File folder = new File("C:/Users/ele_1/ML/NaiveBayes/part1/legit");
+        File[] files = folder.listFiles();
+        for(File f: files) {
+        	 NaiveBayes NBHam = new NaiveBayes(Category.Ham);
+        	System.out.println(f);
+        	Scanner s = new Scanner(f);
+        	ArrayList<String> list = new ArrayList<String>();
+    		while (s.hasNext()){
+    		    list.add(s.next());
+    		}
+    		s.close();
+    		
+    		NBHam.train( list, Category.Ham);
+    		
+        }
+        System.out.println("Press Any Key To Continue...");
+        new java.util.Scanner(System.in).nextLine();
+        
+        
+        
+        System.out.println("*************CLASSIFICATION SECTION***************");
+        System.out.println("*************CLASSIFICATION SECTION***************");
+        System.out.println("*************CLASSIFICATION SECTION***************");
+        System.out.println("********** s p a m *************");
+        File spamFolder1 = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/spam1");
+        File[] spamFiles1 = spamFolder1.listFiles();
+        double NospamFiles=0;
+   		double spamAccuracy = 0;
+   		double hamAccuracy = 0;
+        for(File f: spamFiles1) {
+        	NospamFiles++;
+       	 NaiveBayes NBSpam = new NaiveBayes();
+       	System.out.println(f);
+       	Scanner s = new Scanner(f);
+       	ArrayList<String> list = new ArrayList<String>();
+   		while (s.hasNext()){
+   		    list.add(s.next());
+   		}
+   		s.close();
+   		
+
+   		if(NBSpam.classify(list)== Category.Spam) spamAccuracy++;
+   		else hamAccuracy++;
+
+       }
+   		System.out.println("Number of Spam files examined"+ NospamFiles);
+   		System.out.println("Number of Spam files classified in Spam"+ spamAccuracy);
+        /*
+        System.out.println("********** h a m *************");
+        File spamFolder2 = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/ham1");
+        File[] spamFiles2 = spamFolder2.listFiles();
+        double NohamFiles=0;
+   		double spamAccuracy1 = 0;
+   		double hamAccuracy1 = 0;
+        for(File f: spamFiles2) {
+        	NohamFiles++;
+       	 NaiveBayes NBSpam = new NaiveBayes();
+       	System.out.println(f);
+       	Scanner s = new Scanner(f);
+       	ArrayList<String> list = new ArrayList<String>();
+   		while (s.hasNext()){
+   		    list.add(s.next());
+   		}
+   		s.close();
+   		//System.out.println(NBSpam.classify(list));
+
+   		if(NBSpam.classify(list)== Category.Spam) {
+   			spamAccuracy1++;
+   			System.out.println(f);
+   		}
+   		else hamAccuracy1++;
+   		
+   		System.out.println("Number of Ham files examined"+ NohamFiles);
+   		System.out.println("Number of Ham files classified in Ham"+ hamAccuracy1);
+       }
+
+        */
+        /*
         //File f = new File("C:/Users/ele_1/ML/NaiveBayes/enron1/spam/0697.2004-03-22.GP.spam.txt");
         System.out.println("Training Spam files");
         for(File f: files){
@@ -24,8 +118,27 @@ public class Main {
            NBSpam.train( list, Category.Spam);
             NBSpam.classify(list);
         }
-	
+	*/
 	//b.train(args);
 
 	}
 }
+
+/*
+int i =0 ; 
+for(File f: files){
+	String str ="";
+	Scanner s = new Scanner(f);
+	ArrayList<String> list = new ArrayList<String>();
+	while (s.hasNext()){
+	    str+=s.next()+" ";
+	    
+	}
+	list.add(i,str);
+	i++;
+	s.close();
+	System.out.println(list);
+	System.out.println(list.size());
+	for (int y =0; y<list.size() ;y++){
+		System.out.println("List-get-i"+list.get(y));
+*/
--- NaiveBayes/src/NaiveBayes.java
+++ NaiveBayes/src/NaiveBayes.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 import java.lang.Math;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -10,24 +11,33 @@ import java.io.IOException;
 // Iteration through the whole Folder
 /* *******TEXT PROCESSING*********
 * Prepare the text so that it's suitable for tokenization and stopWord-filtering
-*
+* xDeterminers: the, a, an, another
+* xCoordinating Conjunctions: for, an, nor, but, or, yet, so
+* xPrepositions: in, under, towards, before
 * Add more Words. Ref: https://towardsdatascience.com/machine-learning-text-processing-1d5a2d638958
 * */
 
 public class NaiveBayes {
 	private Category category;
 	private static final String FILENAME = "C:/Users/ele_1/ML/NaiveBayes/0006.2003-12-18.GP.spam.txt";
+	
 	private String[] stopWords = {"about","a","an","like","of","to", "at","on","be","and","is","are","was","com","nor","for","from","how",
             "that","the","it","this","so", "was","what","when","where","who","will","with","und","but","www", "under", "towards",
             "before","in"};
-	ArrayList <String> keyWords = new ArrayList<String>();
+	 
 	Map<ArrayList<String>, Category> trainingSet = new HashMap<>();
-	Map<String, Integer> wordFrequencySpam = new HashMap<>();
-	Map<String, Integer> wordFrequencyHam = new HashMap<>();
+	static Map<String, Integer> wordFrequencySpam = new HashMap<>();
+	static Map<String, Integer> wordFrequencyHam = new HashMap<>();
+	static double spamCount=0;
+	static double hamCount=0;
 	public NaiveBayes() {
 	
 	}
-	public NaiveBayes(Category category) {
+
+	/*
+	 * Unnecessary probably 
+	 */
+	public NaiveBayes(Category category) {  
 		this.setCategory(Category.Spam);
 	}
 	
@@ -38,69 +48,35 @@ public class NaiveBayes {
 		this.category = category;
 	}
 
-
 	public void train(ArrayList<String> list, Category category) {
 		Category spam = Category.Spam;
 		Category ham = Category.Ham;
 		trainingSet.put(list, category);
+	
 		if (category == spam) {
-			keyWords.addAll(tokenize(list, keyWords));
+			spamCount++;
+			ArrayList <String> keyWords = new ArrayList<String>();
+			keyWords.addAll(tokenize(list));
 			//Map<String, Integer> wordFrequency = new HashMap<>();
 			for (String keyword : keyWords) {
 				//System.out.println(keyword);
 				Integer n = wordFrequencySpam.get(keyword);
 				n = (n == null) ? 1 : ++n;
 				wordFrequencySpam.put(keyword, n);
+				//System.out.println("Keyword&noOcc: "+ keyword+n);
+				
 			}
-		}
-		/*
-		BufferedWriter bw = null;
-		FileWriter fw = null;
-
-		try {
-
-			String data = " This is new content";
-
-			File Tfile = new File(FILENAME);
-
-			// if file doesn't exist, then create it
-			if (!Tfile.exists()) {
-				Tfile.createNewFile();
-			}
-
-			// true = append file
-			fw = new FileWriter(Tfile.getAbsoluteFile(), true);
-			bw = new BufferedWriter(fw);
-
-			bw.write(data);
-
-			System.out.println("Done");
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-
-				if (bw != null)
-					bw.close();
-
-				if (fw != null)
-					fw.close();
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
-
-			}
-		}
-
+			//System.out.println("WordFrequency-MAP Contains the following:");
+			//for(String key :wordFrequencySpam.keySet()) {
+			//System.out.println("timeskey:"+wordFrequencySpam.get(key)+ key);
 		
-		*/
+			//}
+		}
+		
 		if (category == ham) {
-			keyWords.addAll(tokenize(list, keyWords));
+			hamCount++;
+			ArrayList <String> keyWords = new ArrayList<String>();
+			keyWords.addAll(tokenize(list)); //, keyWords)
 			//Map<String, Integer> wordFrequency = new HashMap<>();
 			for (String keyword : keyWords) {
 				//System.out.println(keyword);
@@ -111,8 +87,10 @@ public class NaiveBayes {
 		}
 	}
 	public Category classify(ArrayList<String> list) {
-		keyWords = this.tokenize(list, keyWords);
-		category = this.decide(list);
+		ArrayList <String> keyWords = new ArrayList<String>();
+		keyWords = this.tokenize(list);
+		category = this.decide(keyWords);
+		System.out.println("++++++++++++++++++   "+ category+ "   ++++++++++++++++++");
 		return category;
 	}
 
@@ -127,44 +105,55 @@ public class NaiveBayes {
  * the category after applying Naive Bayes Classifier. Laplace
  * smoothing is added.
  */
-	private Category decide(ArrayList<String> list) {
+	private Category decide(ArrayList<String> keyWords) {
 		Category spam = Category.Spam;
 		Category ham = Category.Ham;
 		category = ham; //by def
 		trainingSet.size();
-		double spamCount = 0;
-		double hamCount = 0;
-		for( Map.Entry<ArrayList<String>, Category> entry : trainingSet.entrySet()) {
-			if (entry.getValue() == spam) {
-				spamCount++;
-			}else
-				hamCount++;	
-		}
-		System.out.println("Spam Count:" + spamCount);
-		System.out.println("Ham Count:"+ hamCount);
+
+		//System.out.println("Spam Count:" + spamCount);
+		//System.out.println("Ham Count:"+ hamCount);
 		double totalCount = spamCount + hamCount;
 		//Calculating P(Spam) & P(Ham)
 		double pSpam = spamCount / totalCount;
-		double pHam = spamCount / totalCount;
+		double pHam = hamCount / totalCount;
 		// Laplace Smoothing - Count of distinct words
-		double distinctWords = wordFrequencySpam.size();
+		
+		double common = 0;
+		for( Entry<String, Integer> words: wordFrequencyHam.entrySet()) {
+			if(wordFrequencySpam.get(words)!=null) {
+				common++;
+			}
+		}
+		double distinctWords = wordFrequencySpam.size()+wordFrequencyHam.size() -(2*common );
+		//System.out.println(wordFrequencySpam.size());
+		//System.out.println(wordFrequencyHam.size());
 		// Use Log probability because we're dealing with small numbers
 		//Possible	precision problems ?
 		double emailIsSpam = Math.log(pSpam);
 		double emailIsHam = Math.log(pHam);
+		//System.out.println(emailIsSpam);
+		//System.out.println(emailIsHam);
 		for(String keyword: keyWords) {
-			if (category==spam) {
+			//System.out.println(keyword);
+
 			Integer spamOccur = wordFrequencySpam.get(keyword);
+			if(spamOccur==null) spamOccur = 0;
+			else spamOccur = wordFrequencySpam.get(keyword);
+
+			//System.out.println(spamOccur);
 			emailIsSpam += Math.log((spamOccur+1)/ (spamCount + distinctWords)); // or wordFrequency.size()
-			}
 			
-			if (category==ham) {
+			
 			Integer hamOccur = wordFrequencyHam.get(keyword);
-			emailIsHam += Math.log((hamOccur+1)/ (hamCount + distinctWords));
-			}
+			if(hamOccur==null) hamOccur = 0;
+			else hamOccur = wordFrequencyHam.get(keyword);
 			
+			emailIsHam += Math.log((hamOccur+1)/ (hamCount + distinctWords));
+
 		}
-		
+		//System.out.println("ham"+emailIsHam);
+		//System.out.println("spam"+emailIsSpam);
 		if(emailIsHam>= emailIsSpam) {
 			category = ham;
 		}else {
@@ -182,17 +171,23 @@ public class NaiveBayes {
 	
 	
 	
-	private ArrayList<String> tokenize(ArrayList<String> list, ArrayList<String> keyWords) {
-		System.out.println("Inside tokenizer");
+	private ArrayList<String> tokenize(ArrayList<String> list) { //, ArrayList<String> keyWords)
+		//System.out.println("Inside tokenizer");
+		ArrayList <String> keyWords = new ArrayList<String>();
 		for(int i=0; i<list.size(); i++) {
-			if(list.get(i)=="/[^(a-zA-ZA-Яa-я0-9_)+\\s]" ||list.get(i)=="[/(){}\\[\\]\\|@,;]" ) {
-				list.get(i).replaceAll("[/(){}\\\\[\\\\]\\\\|@,;]", "");
+			String word = list.get(i);
+			//System.out.println(word);
+			if(word=="/[^a-zA-Z 0-9]+/") {
+				word.replaceAll("/[^a-zA-Z 0-9]+/", "");
 			}
-			list.set(i,list.get(i).toLowerCase() ); // gia ta alla synola estw
-			if(list.get(i).length()>=3) {
-				for( String word : stopWords) {
-					if(list.get(i).equals(word)== false) {
-						keyWords.add(list.get(i));
+			list.set(i,word.toLowerCase() ); // gia ta alla synola estw
+			
+			if(word.length()>=3) {
+				//System.out.println("List of 3 letters+:" +word);
+				for( String stopWord : stopWords) {
+					if(word.equals(stopWord)== false) {
+						//System.out.println("What is gonna be added in keyWords:" +word);
+						keyWords.add(word);
 						//System.out.println(keyWords);
 						break;
 					}
@@ -201,7 +196,7 @@ public class NaiveBayes {
 			}
 		}
 		//System.out.println(list.size());
-		System.out.println(keyWords);
+		//System.out.println(keyWords);
 		return keyWords;
 
 
